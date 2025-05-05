c5459dca455975f42e628a16bcc12ae707feeda9
Jibran <jibran.yousuf@eurosofttech.co.uk>
2025-05-05 22:46:54 +0500

    [PrimeTravel][8441- design form and perform insert update of DRIVER BANK DETAILS]

---
+++ Taxi_AppMain/Classes/ClsDriverBankDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi_AppMain.Classes
+{
+    public class ClsDriverBankDetail
+    {
+        public int Id { get; set; }
+        public int DriverId { get; set; }
+        public string SortCode { get; set; }
+        public string AccountNo { get; set; }
+        public string AccountTitle { get; set; }
+        public string BankName { get; set; }
+        public string CompanyNo { get; set; }
+        public string CompanyVatNo { get; set; }
+        public string IbanNo { get; set; }
+        public string BlcNo { get; set; }
+    }
+}
